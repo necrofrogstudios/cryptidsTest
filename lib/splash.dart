1df4b165c6290a6b8eaff54d0008628c8c0c5067
FlutLab.io <>
2021-12-14 11:23:08 +0000

    splash screen duration length changed to 1

--- lib/splash.dart
+++ lib/splash.dart
@@ -12,18 +12,13 @@ class _Splash extends State<Splash> {
   @override
   Widget build(BuildContext context) {
     return new SplashScreen(
-        seconds: 5,
+        seconds: 1,
         navigateAfterSeconds: MyApp(),
         title: new Text(
           'Necrofrog Studios',
-          style: new TextStyle(
-              fontWeight: FontWeight.bold,
-              fontFamily: 'Hind',
-              fontSize: 35.0,
-              color: Colors.white),
+          style: new TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Hind', fontSize: 35.0, color: Colors.white),
         ),
-        image: new Image.network(
-            'https://purepng.com/public/uploads/large/purepng.com-moonmoonastronomical-bodyfifth-largest-natural-satellitenatural-satellitemoon-light-1411527067328btskq.png'),
+        image: new Image.network('https://purepng.com/public/uploads/large/purepng.com-moonmoonastronomical-bodyfifth-largest-natural-satellitenatural-satellitemoon-light-1411527067328btskq.png'),
         photoSize: 150.0,
         backgroundColor: Colors.black,
         styleTextUnderTheLoader: new TextStyle(
