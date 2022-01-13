845960c07d232bfb75dbc76f5211e0b5bc8ade9f
kevinlalik <kevinlalik@gmail.com>
2022-01-12 22:03:10 -0600

    na

--- lib/ads/ad_helper.dart
+++ lib/ads/ad_helper.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return "ca-app-pub-3940256099942544/6300978111"; // CHANGE TO ca-app-pub-1772118121041384/1269617005 FOR GOOGLE PLAY PUBLISHING
+      return "ca-app-pub-1772118121041384/1269617005"; // CHANGE TO ca-app-pub-1772118121041384/1269617005 FOR GOOGLE PLAY PUBLISHING
     } else if (Platform.isIOS) {
       return "ca-app-pub-3940256099942544/2934735716";
     } else {
